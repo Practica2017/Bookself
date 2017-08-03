e01251ae3c16036c76dda48826553775271cae59
Sebastian <filip.seba@gmail.com>
2017-08-03 15:03:07 +0300

    com1

---
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/Book/BestBooksActivity.java
@@ -0,0 +1,19 @@
+package com.example.bookshelfproject.Activity.Book;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.bookshelfproject.R;
+
+/**
+ * Created by filip on 8/3/2017.
+ */
+
+public class BestBooksActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.best_books);
+
+    }
+}
---
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/Model/Book.java
@@ -0,0 +1,81 @@
+package com.example.bookshelfproject.Model;
+
+/**
+ * Created by filip on 8/3/2017.
+ */
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private float score;
+    private int votes;
+    private String description;
+    private String category;
+
+    public Book(String id, String title, String author, float score, int votes, String description, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.score = score;
+        this.votes = votes;
+        this.description = description;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
