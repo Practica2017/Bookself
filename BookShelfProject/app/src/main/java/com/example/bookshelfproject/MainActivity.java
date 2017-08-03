4eba1ee6dbce2942e1e94ef1e605510c4541e7e1
iuliacentea <iuli.centea@gmail.com>
2017-08-03 15:46:31 +0300

    3rd push

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/MainActivity.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
