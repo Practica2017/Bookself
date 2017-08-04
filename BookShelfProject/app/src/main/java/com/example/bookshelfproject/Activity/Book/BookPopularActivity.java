28b8b26777a0dd84e5d5168ffa1ee5e6b5004d6e
Sebastian <filip.seba@gmail.com>
2017-08-04 16:09:23 +0300

    updated bookpopularactivity

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/Book/BookPopularActivity.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/Book/BookPopularActivity.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.bookshelfproject.Model.Book;
+
 import com.example.bookshelfproject.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,8 +33,6 @@ public class BookPopularActivity extends AppCompatActivity {
     private ArrayList<Book> bestBooksBook = new ArrayList<>();
     private ArrayAdapter adapter;
 
-    private FirebaseDatabase firebaseDatabase;
-    private StorageReference mStorageRef;
     private DatabaseReference mDatabase;
 
 
@@ -42,51 +41,47 @@ public class BookPopularActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.best_books);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-       //mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("books").child("1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Book book = dataSnapshot.getValue(Book.class);
+                if(book != null)
+                bestBooksBook.add(1,book);
+                else
+                    Log.d("myTag", "This is my message");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+
+      //  Log.d("myTag", "This is my message" + bestBooksBook.get(1).getTitle());
 
         listView = (ListView) findViewById(R.id.listview);
 
-        for (int i=0;i<10;i++){
-            bestBooks.add(i,"book nr "+i);
+        for (int i = 0; i < 10; i++) {
+            bestBooks.add(i, "book nr " + i);
         }
-
-        Book book = new Book();
-        //mDatabase.child("books").child(1+"").setValue(book);
-
-        /*mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                readBooks(dataSnapshot);
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });*/
-
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,bestBooks);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bestBooks);
         listView.setAdapter(adapter);
-
-
     }
 
     private void readBooks(DataSnapshot dataSnapshot) {
-        for (DataSnapshot ds : dataSnapshot.getChildren()){
-            int i=1;
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            int i = 1;
             Book book = new Book();
-            book.setTitle(ds.child(i+"").getValue(Book.class).getTitle());
-            book.setAuthor(ds.child(i+"").getValue(Book.class).getAuthor());
-            book.setDescription(ds.child(i+"").getValue(Book.class).getDescription());
-            book.setScore(ds.child(i+"").getValue(Book.class).getScore());
-            book.setVotes(ds.child(i+"").getValue(Book.class).getVotes());
-
-            bestBooksBook.add(i,book);
-
-
+            book.setTitle(ds.child(i + "").getValue(Book.class).getTitle());
+            book.setAuthor(ds.child(i + "").getValue(Book.class).getAuthor());
+            book.setDescription(ds.child(i + "").getValue(Book.class).getDescription());
+            book.setScore(ds.child(i + "").getValue(Book.class).getScore());
+            book.setVotes(ds.child(i + "").getValue(Book.class).getVotes());
+            bestBooksBook.add(i, book);
             i++;
-
-
         }
     }
 
