1a8481c047e6e094e44cfbb786fbbb456c75a8c8
Sebastian <filip.seba@gmail.com>
2017-08-04 12:15:43 +0300

    .

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/MainActivity.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/MainActivity.java
@@ -1,8 +1,12 @@
     package com.example.bookshelfproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.example.bookshelfproject.Activity.Book.BookPopularActivity;
 
     public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,13 @@ import android.widget.Button;
 
     private void configureBestBooks() {
         Button bestBooksButton = (Button) findViewById(R.id.bestBooks);
-       // bestBooksButton.setOnClickListener();
+        bestBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BookPopularActivity.class));
+                //finish();
+            }
+        });
     }
 
 
