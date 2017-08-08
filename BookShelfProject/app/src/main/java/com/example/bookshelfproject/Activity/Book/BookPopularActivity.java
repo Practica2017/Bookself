1db6546f763b2c0f6e2fff90bcea5de386b2ea6a
Sebastian <filip.seba@gmail.com>
2017-08-08 10:42:28 +0300

    book popular working

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/Book/BookPopularActivity.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/Book/BookPopularActivity.java
@@ -5,6 +5,7 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,9 +18,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +29,7 @@ public class BookPopularActivity extends AppCompatActivity {
 
     private ListView listView;
     private ArrayList<String> bookTitles = new ArrayList<>();
-    private List<Book> bestBooks = new ArrayList<>();
+    private static List<Book> bestBooks = new ArrayList<>();
     private ArrayAdapter adapter;
 
     private FirebaseDatabase database;
@@ -43,29 +41,38 @@ public class BookPopularActivity extends AppCompatActivity {
         setContentView(R.layout.best_books);
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
+        addBestBooks(databaseReference);
+    }
 
-        /*Book bookTest = new Book(1,"Moara cu Noroc","Ioan Slavici",5,10,"Best book ever","Drama");
-        databaseReference.child("books").child(bookTest.getId()+"").setValue(bookTest);*//*
-        Book bookTest1 = new Book(1,"Moara cu Noroc","Ioan Slavici",5,10,"Best book ever","Drama");
-        databaseReference.child("books").child(bookTest1.getId()+"").setValue(bookTest1);
-        Book bookTest2 = new Book(3,"Fram ursul polar","Ioan Slavici",5,10,"Best book ever","Drama");
-        databaseReference.child("books").child(bookTest2.getId()+"").setValue(bookTest2);
-        Book bookTest3 = new Book(4,"Amintiri din copilarie","Ioan Slavici",5,10,"Best book ever","Drama");
-        databaseReference.child("books").child(bookTest3.getId()+"").setValue(bookTest3);
-        Book bookTest4 = new Book(5,"Ion","Ioan Slavici",5,10,"Best book ever","Drama");
-        databaseReference.child("books").child(bookTest4.getId()+"").setValue(bookTest4);*/
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK))
+        {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
+    private void addBestBooks(DatabaseReference databaseReference) {
+        bestBooks.clear();
+        bookTitles.clear();
         databaseReference.child("books").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> children = dataSnapshot.getChildren();
-                int i=0;
+                int i = 0;
                 for (DataSnapshot child : children) {
                     Book book = child.getValue(Book.class);
-                    bestBooks.add(i,book);
+                    bestBooks.add(i, book);
                     i++;
-                    Log.d("my msh", "----------------------------------------title: "+book.getTitle());
                 }
+                for (i = 0; i < bestBooks.size(); i++) {
+                    bookTitles.add(i, bestBooks.get(i).getTitle());
+                }
+                listView = (ListView) findViewById(R.id.listview);
+                adapter = new ArrayAdapter(BookPopularActivity.this, android.R.layout.simple_list_item_1, bookTitles);
+                listView.setAdapter(adapter);
             }
 
             @Override
@@ -73,15 +80,6 @@ public class BookPopularActivity extends AppCompatActivity {
 
             }
         });
-
-
-        listView = (ListView) findViewById(R.id.listview);
-        for (int i = 0; i < bestBooks.size(); i++) {
-            bookTitles.add(i,bestBooks.get(i).getTitle());
-        }
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bookTitles);
-        listView.setAdapter(adapter);
     }
-
 
 }
