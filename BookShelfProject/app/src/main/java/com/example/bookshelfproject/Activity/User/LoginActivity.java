0b0b784c82800788257cb24c3827bcb954ca184b
Sebastian <filip.seba@gmail.com>
2017-08-09 14:54:55 +0300

    Login and register fully functional

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/User/LoginActivity.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/Activity/User/LoginActivity.java
@@ -2,63 +2,62 @@ package com.example.bookshelfproject.Activity.User;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bookshelfproject.Activity.Book.BookPopularActivity;
 import com.example.bookshelfproject.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button buttonRegister;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button siginButton;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private TextView textViewSignin;
-    private ProgressDialog progressDialog;
+    private TextView textViewRegister;
+    ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        progressDialog = new ProgressDialog(this);
-        buttonRegister = (Button) findViewById(R.id.buttonRegister);
-        buttonRegister.setOnClickListener(this);
+        siginButton = (Button) findViewById(R.id.buttonSignin);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        textViewSignin = (TextView) findViewById(R.id.texViewSignIn);
-        textViewSignin.setOnClickListener(this);
+        textViewRegister = (TextView) findViewById(R.id.texViewRegister);
+        progressDialog = new ProgressDialog(this);
 
+        siginButton.setOnClickListener(this);
+        textViewRegister.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == buttonRegister)
-            registerUser();
-        else if (v == textViewSignin) {
-            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        if(v == siginButton){
+            userLogin();
+        }if (v == textViewRegister){
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             finish();
         }
     }
 
-    private void registerUser() {
+    private void userLogin() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+
         if (email.isEmpty()) {
             Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
             return;
@@ -67,26 +66,20 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
             return;
         }
-        progressDialog.setMessage("Registering User...");
+        progressDialog.setMessage("Wait...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                if (task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this, BookPopularActivity.class));
+                    finish();
                 }else{
-                    Toast.makeText(RegisterActivity.this, "Registration was not successful, please try again", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Not registered. Please register first.", Toast.LENGTH_LONG).show();
                 }
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(RegisterActivity.this, "FAILURE", Toast.LENGTH_SHORT).show();
-            }
         });
-
         progressDialog.hide();
     }
 }
