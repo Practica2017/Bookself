bf3dc1475d2b29aa13e1f960733b464bec7667ca
iuliacentea <iuli.centea@gmail.com>
2017-08-16 14:11:01 +0300

    changes splashscreen

---
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.bookshelfproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.bookshelfproject.Activity.Book.BookPopularActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView = (ImageView) findViewById(R.id.imageView);
+       // Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        final Intent i = new Intent(this, BookPopularActivity.class);
+       // imageView.startAnimation(myanim);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+
+                timer.start();
+    }
+}
