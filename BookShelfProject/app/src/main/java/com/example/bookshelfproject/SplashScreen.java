ec5db2bcf8fd4e965d3bcefbeb33f79900a13088
iuliacentea <iuli.centea@gmail.com>
2017-08-16 15:33:04 +0300

    splashscreen ch

--- BookShelfProject/app/src/main/java/com/example/bookshelfproject/SplashScreen.java
+++ BookShelfProject/app/src/main/java/com/example/bookshelfproject/SplashScreen.java
@@ -23,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         Thread timer = new Thread() {
             public void run() {
                 try {
-                    sleep(4000);
+                    sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
